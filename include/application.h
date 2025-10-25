2ae1e12ef5ac65a2145642ab46fa1d6799ff1c40
D0T-B0X <aadi2399@outlook.com>
2025-10-25 19:36:50 +0530

    added some comments

--- include/application.h
+++ include/application.h
@@ -36,32 +36,33 @@ private:
     Surface surface;
 
     void setupProgram() {
-        ball_one.Name = "Ball One";
-        ball_one.mesh.source = false;
-        ball_one.Color = {1.0f, 0.0f, 0.0f};
-        ball_one.setRadius(0.3f);
+        ball_one.Name = "Ball One";             // Set sphere debug name
+        ball_one.mesh.source = false;           // Set sphere emission status
+        ball_one.Color = {1.0f, 0.0f, 0.0f};    // Set sphere color
+        ball_one.setRadius(0.5f);               // Set sphere size
 
         ball_two.Name = "Ball Two";
         ball_two.mesh.source = false;
         ball_two.Color = {0.0f, 1.0f, 0.0f};
-        ball_two.setRadius(0.3f);
+        ball_two.setRadius(0.5f);
 
         ball_three.Name = "Ball Three";
         ball_three.mesh.source = false;
         ball_three.Color = {0.0f, 0.0f, 1.0f};
-        ball_three.setRadius(0.3f);
+        ball_three.setRadius(0.5f);
 
+        // Light source sphere
         light.Name = "Light";
         light.mesh.source = true;
         light.Color = {1.0f, 1.0f, 1.0f};
-        light.setRadius(0.105f);
+        light.setRadius(0.5f);
 
-        surface.color = glm::vec3(0.5f, 0.5f, 0.5f);
-        surface.setSize(40.0f);
-        surface.setWireframe(true);          // Enable wireframe grid
-        surface.setGridDensity(12, 12);      // Control grid density
-        surface.mesh.inactive = true;
-        surface.setDistance(-2.0f);
+        surface.color = glm::vec3(0.5f, 0.5f, 0.5f); // Surface color
+        surface.setSize(40.0f);                      // Surface size (height and width)
+        surface.setWireframe(true);                  // Enable wireframe grid
+        surface.setGridDensity(12, 12);              // Control grid density
+        surface.mesh.inactive = true;                // Surface will not be affected by lighting
+        surface.setDistance(-2.0f);                  // Distance of surface from origin (y-axis)
 
         rEngine.drawSphere(ball_one, {0.0f, 2.0f, -2.0f});
         rEngine.drawSphere(ball_two, {1.7320508075f, -1.0f, -2.0f});
